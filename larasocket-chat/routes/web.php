f553c9b5f82ddcbccb88adcac1235c1db2249342
Lucas <lucas46em@gmail.com>
2021-08-26 11:02:36 -0300

    trying to create chat with broadcast laravel

--- larasocket-chat/routes/web.php
+++ larasocket-chat/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::view('/', 'chat')->middleware('auth');
+Route::get('messages', [MessageController::class,'index']);
+Route::post('messages', [MessageController::class,'store']); 
