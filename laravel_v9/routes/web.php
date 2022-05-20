c8524051fb5dea660e166ba8fef061998eaf7cde
Lucas <lucas46em@gmail.com>
2022-05-20 17:56:55 -0300

    studing sanctum

--- laravel_v9/routes/web.php
+++ laravel_v9/routes/web.php
@@ -3,7 +3,7 @@
 use App\Jobs\ExampleJob;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,7 +24,28 @@ Route::get('users', function () {
 });
 
 Route::get('create-job', function () {
-    // dispatch(new ExampleJob('lucas','lucas@gmail.com','123456'));
-    ExampleJob::dispatch('lucas','lucas@gmail.com','123456');
+    ExampleJob::dispatch('lucas','lucas@gmail.com',Hash::make('123456'));
     return 'created';
 });
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        // 'device_name' => 'required',//pode ser o segredo
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken('segredo')->plainTextToken;
+})->name('create.token');
