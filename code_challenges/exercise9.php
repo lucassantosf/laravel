fc85dfb41677faac8729146abc6539b35a3f4e46
lucas <lucas46em@gmail.com>
2026-02-14 15:39:43 -0300

    code challenge - longest common prefix

--- code_challenges/exercise9.php
+++ code_challenges/exercise9.php
@@ -1,112 +1,51 @@
 <?php 
 
-function longestCommonPrefix3($strs){
+// Done at: 2026-02-14
 
-    $longest = '';
+// 14. Longest Common Prefix
+// Write a function to find the longest common prefix string amongst an array of strings.
 
-    foreach ($strs as $x => $word) {
-        
-        foreach ($strs as $y => $w) {
+// If there is no common prefix, return an empty string "".
 
-            if($x != $y){
+// Example 1:
 
-                if($word == $w && strlen($word) > strlen($longest)){
-                    $longest = $word;
-                    continue;
-                }else if(strlen($word) > strlen($longest)){
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
 
-                    for ($i=0; $i < strlen($word); $i++) { 
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
 
-                        if(isset($w[$i]) && $word[$i]==$w[$i] && strlen($longest)<=$i){
-                            $longest .= $word[$i];
-                        }else{
-                            break;
-                        }
+// Constraints:
 
-                    }
-                }
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lowercase English letters if it is non-empty.
 
-            }
-        }
+class Solution {
 
-    }
-    return $longest;
-}
+    /**
+     * @param String[] $strs
+     * @return String
+     */
+    function longestCommonPrefix($strs) {
+        $prefix = '';
 
-function longestCommonPrefix5($strs){
-    $longest = '';
+        if (count($strs) == 0) return $prefix;
 
-    if(count($strs) == 1) return $strs[0];
-
-    for ($i=0; $i < count($strs); $i++) { 
-
-        for ($y=1; $y < count($strs); $y++) { 
-
-            if(isset($strs[$y][$i]) && ($strs[$i][$y] == $strs[$y][$i]) && ($y == count($strs) - 1) ){
-                $longest .= $strs[$i][$y];
-            }else if(isset($strs[$y][$i]) && ($strs[$i][$y] != $strs[$y][$i]) ){
-                break 2;
-            }
-        }
-    }
-
-    return $longest;
-}
-
-function longestCommonPrefix4($strs){
-    $longest = '';
-    $equal = true;
-
-    if(count($strs) == 1) return $strs[0];
-
-    for ($i=0; $i < count($strs); $i++) { 
-
-        for ($j=1; $j <= count($strs); $j++) { 
-
-            if(!isset($strs[$i][$j]))
-                break 2;
-
-
-        }
-
-    }
-
-    return $longest;
-}
-
-function longestCommonPrefix($strs){
-    $prefix = '';
-
-    for ($i=0; $i < count($strs); $i++) { 
-        
-        for ($l=0; $l < strlen($strs[$i]); $l++) { 
-
-            for ($j=1; $j <= count($strs); $j++) { 
-
-                if(!isset($strs[$j][$l] ))
-                    break 2;
-
-                if($strs[$i][$l] != $strs[$j][$l])
-                    break 2;
-
-                if(($strs[$i][$l] == $strs[$j][$l]) && ($i == count($strs)-1))
-                    $prefix .= $strs[$i][$l];
-
+        for ($j=0; $j < strlen($strs[0]); $j++) {
+            
+            $char = $strs[0][$j];
+            
+            for ($i=1; $i < count($strs); $i++) { 
+                if(!isset($strs[$i][$j]) || $strs[$i][$j] !== $char)
+                    return $prefix;
             }
 
+            $prefix .= $char;
         }
 
+        return $prefix;
     }
-
-    return $prefix;
 }
-
-$strs = ["flower","flow","flight"];
-// $strs = ["dog","racecar","car"];
-// $strs = ["a"];
-// $strs = ["cir","car"];
-// $strs = ["aaa","aa","aaa"];
-
-$result = longestCommonPrefix($strs);
-
-var_dump($result);
