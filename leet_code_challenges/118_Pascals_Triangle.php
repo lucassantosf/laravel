dad30d3024a2b0b85070eb468d4a60e4ff150e2c
lucas <lucas46em@gmail.com>
2026-03-18 18:26:37 -0300

    finish pascals triangle challenge

--- leet_code_challenges/118_Pascals_Triangle.php
+++ leet_code_challenges/118_Pascals_Triangle.php
@@ -1,5 +1,26 @@
 <?php 
 
+// Done at: 2026-03-18
+
+// 118. Pascal's Triangle
+
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+// Example 1:
+
+// Input: numRows = 5
+// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// Example 2:
+
+// Input: numRows = 1
+// Output: [[1]]
+
+// Constraints:
+
+// 1 <= numRows <= 30
+
 class Solution {
 
     /**
@@ -7,38 +28,30 @@ class Solution {
      * @return Integer[][]
      */
     function generate($numRows) {
-        
-        $return = [];
-        $left = 1;
-        $right = 1;
 
-        if($numRows >= 1)
-            $return[] = [$left];
-        
-        if($numRows >= 2)
-            $return[] = [$left,$right];
-        
-        if($numRows >= 3){
+        $result = [];
 
-            for($i=3;$i<=$numRows;$i++){
+        for ($i = 0; $i < $numRows; $i++) {
+            
+            $row = [1];
 
-                $t = [];
+            if ($i > 0) {
+                $prev = $result[$i - 1];
 
-                foreach($return[$i-1] as $value){
-                    
+                for ($j = 0; $j < count($prev) - 1; $j++){
+                    $row[] = $prev[$j] + $prev[$j + 1];
                 }
 
-
-
+                $row[] = 1;
             }
 
+            $result[] = $row;   
         }
 
-
-        return $return;
+        return $result;      
     }
 }
 
 $solution = new Solution();
-$result = $solution->generate(2);
+$result = $solution->generate(5);
 var_dump($result);
